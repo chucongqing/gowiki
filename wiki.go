70386d1f013121327a8c1e3d76e704350ac5c2d9
ccq_com <chucongqing@gmail.com>
2019-11-26 15:37:20 +0800

    add echo web framework and go mod

--- wiki.go
+++ wiki.go
@@ -3,13 +3,30 @@ package main
 import (
 	_ "fmt"
 
-	"github.com/gowiki/server"
+	// "rsc.io/quote"
+	_ "github.com/gowiki/server"
+	// "github.com/gowiki/echoserver"
+
+	"net/http"
+	"github.com/labstack/echo/v4"
 )
+
+func runServer(){
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	e.Logger.Fatal(e.Start(":4399"))
+}
 
 func main() {
 	// p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
 	// p1.save()
 	// p2, _ := loadPage("TestPage")
 	// fmt.Println(string(p2.Body))
-	server.Run()
+	// quote.Hello()
+	runServer();
+	// server.Run()
 }
+
---
+++ wiki_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	// p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+	// p1.save()
+	// p2, _ := loadPage("TestPage")
+	// fmt.Println(string(p2.Body))
+	// server.Run()
+	return
+}
